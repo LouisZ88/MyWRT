1eb696e5e18e3e262bf22b1df2ccc9645459e8f5
Louis Zhang <48829098+LouisZ88@users.noreply.github.com>
2022-05-21 19:15:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,7 +23,6 @@
 # 主题
 git clone https://github.com/kenzok78/luci-theme-argonne.git package/luci-theme-argonne
 git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
-git clone https://github.com/kiddin9/openwrt-packages/trunk/luci-theme-edge package/luci-theme-edge
 
 ##############################################################################################
 
