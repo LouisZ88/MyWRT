b25242d34c1dc9b55daf7e511366a036a1352c4b
Louis Zhang <48829098+LouisZ88@users.noreply.github.com>
2022-07-10 13:32:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -125,7 +125,5 @@ svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-openvpn-server
 #IPTV related
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-oscam package/luci-app-oscam
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-iptvhelper package/luci-app-iptvhelper
-#Edge Theme
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-theme-edge package/luci-theme-edge
 
 
