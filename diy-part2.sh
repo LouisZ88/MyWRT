87b88677274a1a5e3678d946b1f6772b5ff22421
Louis Zhang <48829098+LouisZ88@users.noreply.github.com>
2022-07-10 13:33:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,10 +13,10 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 # 1.更改默认IP
 sed -i 's/192.168.1.1/10.200.88.1/g' package/base-files/files/bin/config_generate
-# sed -i 's/Openwrt/MyWRT/g' package/base-files/files/bin/config_generate
 
 # 2.清除默认密码/改密码为 
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
 # 修改版本名字
 sed -i 's/OpenWrt/LouisWrt/g' package/base-files/files/bin/config_generate
 
