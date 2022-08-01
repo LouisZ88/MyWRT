b0592a1e154333beba6150aa362e7b94ca7516f5
Louis Zhang <48829098+LouisZ88@users.noreply.github.com>
2022-08-01 13:08:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -107,4 +107,5 @@ svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-nps package/lu
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-control-weburl package/luci-app-control-weburl
 #Openvpn server
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-openvpn-server package/luci-app-openvpn-server
-
+#MOSDNS
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-mosdns package/luci-app-mosdns
